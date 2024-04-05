90bda73046a9d8c9e5dc1e576b057946eb2ab001
nshivachev <shivachev.nikolay@gmail.com>
2024-04-05 11:08:56 +0300

    Mobile Web Application skeleton

---
+++ src/main/java/org/softuni/mobilewebapplication/SpringFundamentalsMobileWebApplication.java
@@ -0,0 +1,13 @@
+package org.softuni.mobilewebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFundamentalsMobileWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFundamentalsMobileWebApplication.class, args);
+	}
+
+}
